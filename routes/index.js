adf3c0f0fe79e0e243a4f367c8e2a7404632ccb9
vamos91 <acardnicolas@hotmail.com>
2022-10-04 10:38:46 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', (req, res) => {
+  res.json({response: 'ok'})
+})
+
 module.exports = router;
